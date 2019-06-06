f4bfdcc476af87787e80627cf3d67c0010364d71
amantina05 <44170761+amantina05@users.noreply.github.com>
2019-06-06 14:56:40 -0400

    Add files via upload

---
+++ Lib/lib.js
@@ -0,0 +1,145 @@
+// $('#magazine').turn({gradients: true, acceleration: true});
+
+
+// window.onload = function(){
+//    document.getElementById('send').onclick = function(e){
+//        alert(document.getElementById("name").value);
+//        return false;
+//    }
+// }
+
+
+// function button1() {
+//   let array = []
+//   let book = document.getElementById('book').text
+//   array.push(book)
+//   //create two more variables & make sure they classes in the html
+//   let campaign = document.getElementById('campaign').text
+//   let movie = document.getElementById('movie').text
+//
+//   console.log(array[array.length-1])
+//
+// }
+//
+//
+// button1()
+
+
+// $("button").click(function(){
+//   $("p").toggle();
+// });
+
+// $("button").click(function(){
+//   $("p").hide();
+// });
+//
+// $("button").click(function(){
+//   $("p").show();
+// });
+
+$("button").click(function(){
+  $("p").slideToggle();
+});
+
+
+// // Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myBtn").onclick = function() {myFunction()};
+// function myFunction() {
+//   document.getElementById("myDropdown").classList.toggle("none");
+// }
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+//trashhhhhhh
+// function myFunction() {
+//   document.getElementById("demop2").innerHTML = "TEST TEST TEST";
+//   console.log("works")
+// }
+
+// myFunction();
+// function bigImg(x) {
+//   x.style.height = "50px";
+//   x.style.widgth = "50px";
+//   document.getElementById("demo").innerHTML = "hello";
+//   document.getElementById("demoP").innerHTML = "This is book1";
+//   console.log("works")
+// }
+//
+// bigImg()
+//
+// function normalImg(x) {
+//   x.style.height = "30px";
+//   x.style.widgth = "30px";
+//   document.getElementById("demo").innerHTML = "hello";
+//   document.getElementById("demoP").innerHTML = "";
+// }
+//
+// normalImg()
+
+
+
+// console.log('works')
+
+
+
+document.getElementById("myDetails").addEventListener("toggle", myFunction);
+
+function myFunction1() {
+  alert("In the New Jim Crow, civil rights lawyer Michelle Alexander makes the case that the the system of Jim Crow never died. It jsut took a new form in the shape of mass incarceration. Today, African American men are labelled 'criminals' and stripped of their freedom, their voting rights, and their access to government programs. The original Jim Crow, laws date back to the Reconstruction Era immediately following the Civil War. As soon as freed slaves were given rights, white politicians and former slave owners began imposing a series of laws that segregated African Americans and stripped them of their cibil liberties, including the right to vote. <br> Following the Civil Rights Movement of the 1960s, Richard Nixon declared a 'War on Drugs.' Since then, 'three strikes' laws and harsh mandatory minimums for minor drug offenses have resulted in a wave of mass incarceration that has affected African American more than any other group.");
+}
+
+
+
+
+// Alexander points to a series of Supreme Court decisions that have reinforced this racially prejudiced system. Police officers are now given wide discretion to stop and search virtually anyone they suspect of drug-related crime, and internalized racial biases lead officers to disproportionately target people of color.
+
+
+function myFunction2() {
+  alert("In her memoir, a work of deep reflection and mesmerizing storytelling, Michelle Obama invites readers into her world, chronicling the experiences that have shaped her—from her childhood on the South Side of Chicago to her years as an executive balancing the demands of motherhood and work, to her time spent at the world’s most famous address. With unerring honesty and lively wit, she describes her triumphs and her disappointments, both public and private, telling her full story as she has lived it—in her own words and on her own terms. Warm, wise, and revelatory, Becoming is the deeply personal reckoning of a woman of soul and substance who has steadily defied expectations—and whose story inspires us to do the same.");
+}
+
+
+function myFunction3() {
+  alert("Jackson explains that faith and religion never resonated within him until he was introduced to Buddhism.  Like many of us, Jackson expresses that in order for him [humankind] to fully place his belief in anything, he needs to both feel and relate to the virtues which define the governing belief being presented to him.  He elaborates by explaining that not until he began regular meditation, which Zen Buddhism is based solely upon, was he able to find its tangibility, truth and divinity. \
+He presents three lessons, which were passed on to him by Red Holzman, coach of the New York Knicks during Jackson’s freshman year as an NBA player. The following lessons are not fastidiously focused on Eastern philosophy yet provided Jackson with the ability to tie his newly found spiritual awareness to nearly any universal event. (1) Don’t let anger or heavy objects cloud the mind. (2) Awareness is everything. (3) The power of “We” is stronger than the power of “Me.” Jackson eloquently asks the audience to simply listen to him apply the tenets and theories of Buddhism to the tenets and theories of the practice of everyday life.");
+}
+
+
+function myFunction4() {
+  alert("Keith Ferrazzi learned about the power personal relationships can have early on in his life. Not only did he observe how people interact and reciprocate favors as a caddie in the local country club, but also learned that moxie pays off, when his Dad went to his boss’s boss’s boss’s boss – the CEO of the steel company he worked for – to tell him he wanted more for his son than he ever had. As a result, Keith went to the best private school in the country on a full scholarship.\
+Nowadays Keith’s rolodex counts well beyond 10,000 contacts, whom he can count on to return his calls. In 2005, he shared everything he’d learned so far about being a good networker in Never Eat Alone.\ Even back in 2005, over half of all jobs were found through personal contacts, with only 20% resulting from applying to advertisements and just 10% going to people as a result of unsolicited applications. By now, over 80% of jobs are landed through networking, so a few tutoring lessons in this subject you never had in school can’t hurt, can they?\ Here are 3 lessons to help you become a better networker:\ Relationships aren’t like cake, they’re like muscles.\ You must build your network long before you need it.\ How you spend time with people is much more important than how much time you spend with them.");
+}
+
+
+function myFunction5() {
+  alert("Hillbilly Elegy is a passionate and personal analysis of a culture in crisis—that of white working-class Americans. The disintegration of this group, a process that has been slowly occurring now for more than forty years, has been reported with growing frequency and alarm, but has never before been written about as searingly from the inside. J. D. Vance tells the true story of what a social, regional, and class decline feels like when you were born with it hung around your neck.\
+  The Vance family story begins hopefully in postwar America. J. D.’s grandparents were “dirt poor and in love,” and moved north from Kentucky’s Appalachia region to Ohio in the hopes of escaping the dreadful poverty around them. They raised a middle-class family, and eventually one of their grandchildren would graduate from Yale Law School, a conventional marker of success in achieving generational upward mobility. But as the family saga of Hillbilly Elegy plays out, we learn that J.D.'s grandparents, aunt, uncle, sister, and, most of all, his mother struggled profoundly with the demands of their new middle-class life, never fully escaping the legacy of abuse, alcoholism, poverty, and trauma so characteristic of their part of America. With piercing honesty, Vance shows how he himself still carries around the demons of his chaotic family history.\
+   A deeply moving memoir, with its share of humor and vividly colorful figures, Hillbilly Elegy is the story of how upward mobility really feels. And it is an urgent and troubling meditation on the loss of the American dream for a large segment of this country.");
+}
+
+
+function myFunction6() {
+  alert("For the first time Gucci Mane tells his story in his own words. It is the captivating life of an artist who forged an unlikely path to stardom and personal rebirth. Gucci Mane began writing his memoir in a maximum-security federal prison. Released in 2016, he emerged radically transformed. He was sober, smiling, focused, and positive—a far cry from the Gucci Mane of years past.\ Born in rural Bessemer, Alabama, Radric Delantic Davis became Gucci Mane in East Atlanta, where the rap scene is as vibrant as the dope game. His name was made as a drug dealer first, rapper second. His influential mixtapes and street anthems pioneered the sound of trap music. He inspired and mentored a new generation of artists and producers: Migos, Young Thug, Nicki Minaj, Zaytoven, Mike Will Made-It, Metro Boomin.\
+Yet every success was followed by setback. Too often, his erratic behavior threatened to end it all. Incarceration, violence, rap beefs, drug addiction. But Gucci Mane has changed, and he’s decided to tell his story.\In his extraordinary autobiography, the legend takes us to his roots in Alabama, the streets of East Atlanta, the trap house, and the studio where he found his voice as a peerless rapper. He reflects on his inimitable career and in the process confronts his dark past—years behind bars, the murder charge, drug addiction, career highs and lows—the making of a trap god. It is one of the greatest comeback stories in the history of music.");
+}
+
+
+function myFunction7() {
+  alert("In 2009, when Raquel Cepeda almost lost her estranged father to heart disease, she was terrified she’d never know the truth about her ancestry. Every time she looked in the mirror, Cepeda saw a mystery—a tapestry of races and ethnicities that came together in an ambiguous mix. With time running out, she decided to embark on an archaeological dig of sorts by using the science of ancestral DNA testing to excavate everything she could about her genetic history.\
+ Digging through memories long buried, she embarks upon a journey not only into her ancestry but also into her own history. Born in Harlem to Dominican parents, she was sent to live with her maternal grandparents in the Paraíso (Paradise) district in Santo Domingo while still a baby. It proved to be an idyllic reprieve in her otherwise fraught childhood. Paraíso came to mean family, home, belonging. When Cepeda returned to the US, she discovered her family constellation had changed. Her mother had a new, abusive boyfriend, who relocated the family to San Francisco. When that relationship fell apart, Cepeda found herself back in New York City with her father and European stepmother: attending tennis lessons and Catholic schools; fighting vicious battles wih her father, who discouraged her from expressing the Dominican part of her hyphenated identity; and immersed in the ’80s hip-hop culture of uptown Manhattan. It was in these streets, through the prism of hip-hop and the sometimes\
+loving embrace of her community, that Cepeda constructed her own identity.\Years later, when Cepeda had become a successful journalist and documentary filmmaker, the strands of her DNA would take her further, across the globe and into history. Who were her ancestors? How did they—and she—become Latina? Her journey, as the most unforgettable ones often do, would lead her to places she hadn’t expected to go. With a vibrant lyrical prose and fierce honesty, Cepeda parses concepts of race, identity, and ancestral DNA among Latinos by using her own Dominican-American story as one example, and in the process arrives at some sort of peace with her father.");
+}
+
+
+function myFunction8() {
+  alert("In case you’ve never heard of Shonda Rhimes, she’s the mastermind behind blockbuster TV shows like Grey’s Anatomy and Private Practice. Being an introverted workaholic, most of her public image came from the empowering statements she made about minorities (with a focus on women, and black women in particular), for example by finally putting them in the spotlight in her TV shows with lead roles.\On the inside, she wasn’t happy though. A hurtful, but true comment at a Thanksgiving dinner woke her up in late 2013. “You never say yes to anything.”\For the following year, Shonda decided she’d say yes to anything – and what she learned along the way ended up in this book.\Here are 3 lessons from Year of Yes:\Sometimes you need other people to push you, even if you don’t want to.\No (wo)man is an island: say yes to things without asking too many questions.\Just accept compliments, without playing it down.\Hope you’ve been practicing your “yes”, you’ll need it!");
+}
+
+
+function myFunction9() {
+  alert("Cora is a slave on a cotton plantation in Georgia. Life is hell for all the slaves, but especially bad for Cora; an outcast even among her fellow Africans, she is coming into womanhood—where even greater pain awaits. When Caesar, a recent arrival from Virginia, tells her about the Underground Railroad, they decide to take a terrifying risk and escape. Matters do not go as planned—Cora kills a young white boy who tries to capture her. Though they manage to find a station and head north, they are being hunted.\
+  In Whitehead’s ingenious conception, the Underground Railroad is no mere metaphor—engineers and conductors operate a secret network of tracks and tunnels beneath the Southern soil. Cora and Caesar’s first stop is South Carolina, in a city that initially seems like a haven. But the city’s placid surface masks an insidious scheme designed for its black denizens. And even worse: Ridgeway, the relentless slave catcher, is close on their heels. Forced to flee again, Cora embarks on a harrowing flight, state by state, seeking true freedom.\
+Like the protagonist of Gulliver’s Travels, Cora encounters different worlds at each stage of her journey—hers is an odyssey through time as well as space. As Whitehead brilliantly re-creates the unique terrors for black people in the pre–Civil War era, his narrative seamlessly weaves the saga of America from the brutal importation of Africans to the unfulfilled promises of the present day. The Underground Railroad is at once a kinetic adventure tale of one woman’s ferocious will to escape the horrors of bondage and a shattering, powerful meditation on the history we all share.");
+}
